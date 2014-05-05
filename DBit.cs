144869b2559fa107ef49e56cffcfaa6c7c97a29e
MaxOlsson <maxolsson.mail@gmail.com>
2014-05-05 11:37:27 +0200

    Update DBit.cs

--- DBit.cs
+++ DBit.cs
@@ -1,17 +1,25 @@
 /*
- *      Copyright 2013, Max Olsson
- *
- *      Licensed under the Apache License, Version 2.0 (the "License");
- *      you may not use this file except in compliance with the License.
- *      You may obtain a copy of the License at
+       The MIT License (MIT)
 
- *          http://www.apache.org/licenses/LICENSE-2.0
+        Copyright (c) 2013, Max Olsson
 
- *      Unless required by applicable law or agreed to in writing, software
- *      distributed under the License is distributed on an "AS IS" BASIS,
- *      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *      See the License for the specific language governing permissions and
- *      limitations under the License.
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in
+        all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+        THE SOFTWARE.
  */
 
 using System;
@@ -23,7 +31,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-namespace DBitNET.DBit
+namespace DBitNET
 {
     public class DBit : IDisposable
     {
@@ -31,6 +39,11 @@ namespace DBitNET.DBit
         public DBit(SqlConnection Connection)
         {
             this.Connection = Connection;
+        }
+
+        public DBit(string Connection)
+        {
+            this.Connection = new SqlConnection(Connection);
         }
 
         public SqlCommand Command { get; set; }
@@ -557,17 +570,15 @@ namespace DBitNET.DBit
 
         public static dynamic Populate(DataRow row)
         {
-            if (row[0].GetType().IsPrimitive || row[0].GetType().Name == "String") { return row[0]; }
-
             return new DBObject(row);
         }
         public static T Populate<T>(DataRow row)
         {
             T instance = (T)Activator.CreateInstance(typeof(T));
-            
-            if (typeof(T).IsPrimitive || typeof(T).Name == "String") 
-            { 
-                return (T)row[0]; 
+
+            if (typeof(T).IsPrimitive || typeof(T).Name == "String")
+            {
+                return (T)row[0];
             }
 
             PropertyInfo[] pinfo = instance.GetType().GetProperties();
